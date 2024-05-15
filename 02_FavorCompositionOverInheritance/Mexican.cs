8526232b9abc1c56af3534cd12af2c4cf1e0aae7
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-05-15 07:21:03 +0300

    Favor Composition Over Inheritance

---
+++ 02_FavorCompositionOverInheritance/Mexican.cs
@@ -0,0 +1,8 @@
+﻿namespace _02_FavorCompositionOverInheritance
+{
+    class Mexican : Pizza
+    {
+        public override string Title => $"{base.Title} {nameof(Mexican)}"; // Pizza Cheese
+        public override decimal Price => base.Price + 3m;
+    }
+}
