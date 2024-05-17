334b87387caed7ff30761a00ab4f426a1a487e93
Mohamed Badwy Elsaid <115591574+MohamedBadwy360@users.noreply.github.com>
2024-05-17 09:58:19 +0300

    Delete 02_FavorCompositionOverInheritance/Mexican.cs

--- 02_FavorCompositionOverInheritance/Mexican.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace _02_FavorCompositionOverInheritance
-{
-    class Mexican : Pizza
-    {
-        public override string Title => $"{base.Title} {nameof(Mexican)}"; // Pizza Cheese
-        public override decimal Price => base.Price + 3m;
-    }
-}
