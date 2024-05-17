fb8e669d148e7aa27182586317694c364fa0afd8
Mohamed Badwy Elsaid <115591574+MohamedBadwy360@users.noreply.github.com>
2024-05-17 09:58:06 +0300

    Delete 02_FavorCompositionOverInheritance/Chicken.cs

--- 02_FavorCompositionOverInheritance/Chicken.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace _02_FavorCompositionOverInheritance
-{
-    class Chicken : Pizza
-    {
-        public override string Title => $"{base.Title} {nameof(Chicken)}"; // Pizza Cheese
-        public override decimal Price => base.Price + 6m;
-    }
-}
